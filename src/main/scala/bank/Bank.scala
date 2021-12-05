1faa520024b039a70cf280bb5e865884ca3968f6
mtumilowicz <michal.tumilowicz01@gmail.com>
2021-12-05 17:46:05 +0100

    Bank

--- src/main/scala/bank/Bank.scala
+++ src/main/scala/bank/Bank.scala
@@ -1,17 +1,19 @@
 package bank
 
 import akka.actor.typed.receptionist.{Receptionist, ServiceKey}
+import akka.actor.typed.scaladsl.AskPattern.Askable
 import akka.actor.typed.scaladsl.{ActorContext, Behaviors}
 import akka.actor.typed.{ActorRef, ActorSystem, Behavior}
 import akka.util.Timeout
-import bank.Bank.{BankOperations, CreateAccount, CreditAccountById, find}
+import bank.Bank.{BalanceResponse, BankOperations, CreateAccount, CreditAccountById, GetBalanceById, find}
 
 import java.util.concurrent.TimeUnit
-import scala.util.{Failure, Success}
+import scala.util.Success
 
 sealed trait AccountOperations
 
 case class CreditAccount(amount: Int) extends AccountOperations
+case class GetBalance(replyTo: ActorRef[BalanceResponse]) extends AccountOperations
 
 case class Account(id: String) {
 
@@ -23,6 +25,9 @@ case class Account(id: String) {
     case CreditAccount(amount) =>
       println(s"Account with id = $id was credited with $amount")
       behavior(balance + amount)
+    case GetBalance(sender) =>
+      sender ! BalanceResponse(balance)
+      Behaviors.same
   }
 }
 
@@ -33,37 +38,42 @@ object Bank {
   sealed trait AccountStateOperations extends BankOperations
 
   case class CreditAccountById(id: String, amount: Int) extends AccountStateOperations
+  case class GetBalanceById(id: String) extends AccountStateOperations
+  case class BalanceResponse(balance: Int) extends AccountStateOperations
 
   sealed trait AccountsManagementOperations extends BankOperations
 
   case class CreateAccount(id: String) extends AccountsManagementOperations
 
   private case class AccountToCredit(account: ActorRef[AccountOperations], amount: Int) extends AccountStateOperations
+  private case class AccountToGetBalance(account: ActorRef[AccountOperations]) extends AccountStateOperations
 
   private case class AccountCreditFailed(reason: String) extends AccountStateOperations
 
-  private case class FindFailed(reason: String) extends BankOperations
-
-  def apply(): Behavior[BankOperations] = Behaviors.setup { context =>
-    Behaviors.receiveMessagePartial(
-    accountStateOps(context).orElse {
-      case FindFailed(reason) =>
-        println(reason)
-        Behaviors.same
-      case operations: AccountsManagementOperations => operations match {
-        case CreateAccount(id) =>
-          val account = Account(id)
-          val accountRef = context.spawn(account.behavior(), s"Account-$id")
-          context.system.receptionist ! Receptionist.Register(account.serviceKey, accountRef)
-          Behaviors.same
-      }
+  def apply(): Behavior[BankOperations] = Behaviors.receive { (context, message) =>
+    message match {
+      case operations: AccountStateOperations => accountStateOps(context)(operations)
+      case operations: AccountsManagementOperations => accountManagementOps(context)(operations)
     }
-    )
   }
 
-  private def accountStateOps(context: ActorContext[BankOperations]): PartialFunction[BankOperations, Behavior[BankOperations]] = {
+  private def accountManagementOps(context: ActorContext[BankOperations]): AccountsManagementOperations => Behavior[BankOperations] = {
+      case CreateAccount(id) =>
+        val account = Account(id)
+        val accountRef = context.spawn(account.behavior(), s"Account-$id")
+        context.system.receptionist ! Receptionist.Register(account.serviceKey, accountRef)
+        Behaviors.same
+  }
+
+  private def accountStateOps(context: ActorContext[BankOperations]): AccountStateOperations => Behavior[BankOperations] = {
     case AccountToCredit(account, amount) =>
       account ! CreditAccount(amount)
+      Behaviors.same
+    case AccountToGetBalance(account) =>
+      account ! GetBalance(context.self)
+      Behaviors.same
+    case BalanceResponse(balance) =>
+      println(balance)
       Behaviors.same
     case AccountCreditFailed(reason) =>
       println(reason)
@@ -72,6 +82,11 @@ object Bank {
       case CreditAccountById(id, amount) =>
 
         find(id, context, _.headOption.map(AccountToCredit(_, amount))
+          .getOrElse(AccountCreditFailed(s"account with id = $id does not exist")))
+
+        Behaviors.same
+      case GetBalanceById(id) =>
+        find(id, context, _.headOption.map(AccountToGetBalance)
           .getOrElse(AccountCreditFailed(s"account with id = $id does not exist")))
 
         Behaviors.same
@@ -89,8 +104,6 @@ object Bank {
     ) {
       case Success(listing) =>
         f(listing.serviceInstances(serviceKey))
-      case Failure(exception) =>
-        FindFailed(exception.getMessage)
     }
   }
 
@@ -106,6 +119,7 @@ object Main extends App {
   system ! CreditAccountById("1", 100)
   system ! CreditAccountById("2", 150)
   system ! CreditAccountById("3", 99)
+  system ! GetBalanceById("1")
 
   system.terminate()
 }
