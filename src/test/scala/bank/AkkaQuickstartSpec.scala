71b8b4b2e51f778db4a2001b53cf0031a4fa6c5c
mtumilowicz <michal.tumilowicz01@gmail.com>
2021-12-05 18:55:36 +0100

    tests

---
+++ src/test/scala/bank/AkkaQuickstartSpec.scala
@@ -0,0 +1,26 @@
+//#full-example
+package bank
+
+import akka.actor.testkit.typed.scaladsl.ScalaTestWithActorTestKit
+import bank.Bank.{CreateAccount, GetBalanceById}
+import com.example.Greeter.{Greet, Greeted}
+import org.scalatest.wordspec.AnyWordSpecLike
+
+//#definition
+class AkkaQuickstartSpec extends ScalaTestWithActorTestKit with AnyWordSpecLike {
+//#definition
+
+  "A Greeter" must {
+    //#test
+    "reply to greeted" in {
+      val replyProbe = createTestProbe[BalanceResponse]()
+      val underTest = spawn(Bank())
+      underTest ! CreateAccount("1")
+      underTest ! GetBalanceById("1", replyProbe.ref)
+      replyProbe.expectMessage(BalanceResponse("1", 0))
+    }
+    //#test
+  }
+
+}
+//#full-example
