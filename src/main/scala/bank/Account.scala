a56df54e2d795e18dc6baaa23eee170be68094e5
mtumilowicz <michal.tumilowicz01@gmail.com>
2023-03-05 12:37:49 +0100

    Account actor -> stateless to stateful

--- src/main/scala/bank/Account.scala
+++ src/main/scala/bank/Account.scala
@@ -9,37 +9,45 @@ object AccountProtocol {
   sealed trait AccountOperation
 
   sealed trait AccountCommand extends AccountOperation
+
   case class Credit(amount: NonNegativeInt) extends AccountCommand
+
   case class Debit(amount: NonNegativeInt, replyTo: ActorRef[Either[InsufficientFundsForDebit, Debited]]) extends AccountCommand
+
   case class Debited(id: AccountId, amount: NonNegativeInt)
+
   case class InsufficientFundsForDebit(id: AccountId, balance: NonNegativeInt)
 
   sealed trait AccountQuery extends AccountOperation
+
   case class GetBalance(replyTo: ActorRef[Balance]) extends AccountQuery
+
   case class Balance(id: AccountId, balance: NonNegativeInt)
 
 }
 
 case class Account(id: AccountId) {
 
+  private var state = AccountState(NonNegativeInt(0))
+
   private case class AccountState(balance: NonNegativeInt)
 
   val serviceKey: ServiceKey[AccountOperation] = ServiceKey[AccountOperation](id.raw)
 
-  def behavior(): Behavior[AccountOperation] = behavior(AccountState(NonNegativeInt(0)))
-
-  private def behavior(implicit state: AccountState): Behavior[AccountOperation] = Behaviors.receiveMessage {
+  def behavior(): Behavior[AccountOperation] = Behaviors.receiveMessage {
     case command: AccountCommand => handleCommand(command)
     case query: AccountQuery => handleQuery(query)
   }
 
-  private def handleCommand(command: AccountCommand)(implicit state: AccountState): Behavior[AccountOperation] =
+  private def handleCommand(command: AccountCommand): Behavior[AccountOperation] =
     command match {
-      case Credit(amount) => behavior(state.copy(balance = state.balance + amount))
+      case Credit(amount) =>
+        state = state.copy(balance = state.balance + amount)
+        Behaviors.same
       case debit@Debit(_, _) => handleDebit(debit)
     }
 
-  private def handleDebit(debit: Debit)(implicit state: AccountState): Behavior[AccountOperation] = {
+  private def handleDebit(debit: Debit): Behavior[AccountOperation] = {
     val Debit(amount, replyTo) = debit
     if (state.balance < amount) {
       replyTo ! Left(InsufficientFundsForDebit(id, state.balance))
@@ -47,11 +55,12 @@ case class Account(id: AccountId) {
     } else {
       if (amount.raw > 1_000_000) throw new RuntimeException("You are a big spender, try to spend less!")
       replyTo ! Right(Debited(id = id, amount = amount))
-      behavior(state.copy(balance = state.balance - amount))
+      state = state.copy(balance = state.balance - amount)
+      Behaviors.same
     }
   }
 
-  private def handleQuery(query: AccountQuery)(implicit state: AccountState): Behavior[AccountOperation] =
+  private def handleQuery(query: AccountQuery): Behavior[AccountOperation] =
     query match {
       case GetBalance(replyTo) =>
         replyTo ! Balance(id, state.balance)
